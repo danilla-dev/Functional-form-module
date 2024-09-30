e6428471b0f703c47e4084b514c0b0a9360b609a
Danilla <122106121+danilla-dev@users.noreply.github.com>
2024-09-30 19:26:17 +0200

    Create a first templates of sections.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import { useState } from 'react'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+import { Grid, GridItem } from '@chakra-ui/react'
+
 import './App.css'
 
 function App() {
 	return (
-		<>
-			<h1>App</h1>
-		</>
+		<Grid templateRows='repeat(100vh, 1fr, 200px)' gap={1} minH='100vh'>
+			<GridItem w='100%' h='100vh'>
+				<Header />
+			</GridItem>
+			<GridItem w='100%' minH='100vh'>
+				<Main />
+			</GridItem>
+			<GridItem w='100%' h='200px'>
+				<Footer />
+			</GridItem>
+		</Grid>
 	)
 }
 
