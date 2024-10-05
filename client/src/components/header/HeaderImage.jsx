527a343102213b8e37a9c4aa86587dfcbc2ee770
Danilla <122106121+danilla-dev@users.noreply.github.com>
2024-10-05 20:25:39 +0200

    Get local fonts and optymalize header for highlihts. Next step.

--- client/src/components/header/HeaderImage.jsx
+++ client/src/components/header/HeaderImage.jsx
@@ -5,7 +5,7 @@ import robotAI from '../../assets/AIRobot.svg'
 const HeaderImage = ({ isDesktop }) => {
 	return (
 		<Box w='100%' maxW={isDesktop ? '40%' : 350}>
-			<Image src={robotAI} h='100%' w='100%' filter='drop-shadow(2px 2px 2px #5162FF56)' />
+			<Image src={robotAI} h='100%' w='100%' filter='drop-shadow(2px 2px 2px #5162FF56)' objectFit='cover' />
 		</Box>
 	)
 }
